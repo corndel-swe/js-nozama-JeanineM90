4e4383df5141d712193c56872d79b6e0c5748ab4
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-07-19 10:32:49 +0100

    d2e1

--- db/queries/01.sql
+++ db/queries/01.sql
@@ -2,7 +2,7 @@
 
 -- Get products with their categories
 
-SELECT p.name, c.name
+SELECT p.name, c.name, 
 FROM products p
 JOIN product_categories pc ON p.id = pc.productId
 JOIN categories c ON pc.categoryId = c.id
